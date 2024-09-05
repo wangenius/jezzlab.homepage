765da6b98b7657dadcd2e4a95112938d40a7ced4
wangenius <wangenius@qq.com>
2024-09-05 22:37:49 +0800

    Deploy website - based on 2119aba4277b2e1dadd810c0cd6a82e5e7664319

---
+++ assets/js/b46b057b.8decf8d5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgc_docs=self.webpackChunkgc_docs||[]).push([[31],{9636:(e,s,c)=>{c.r(s),c.d(s,{default:()=>a});var t=c(4848);const a=e=>{let{children:s}=e;return(0,t.jsx)("p",{className:"text-sm sm:text-base  max-w-xl mt-6",children:s})}}}]);
