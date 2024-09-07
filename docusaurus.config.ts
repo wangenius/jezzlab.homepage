7c831910c7428be88eb6cfbecfc67a3b8843cc6b
wangenius <wangenius@qq.com>
2024-09-07 18:17:42 +0800

    init

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,4 +1,3 @@
-import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
@@ -23,7 +22,7 @@ const config: Config = {
   },
   presets: [
     [
-      "classic",
+      "@docusaurus/preset-classic",
       {
         docs: {
           sidebarPath: "./sidebars.ts",
